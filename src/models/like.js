69ebd4e3002b7b1c8a818da200d63be80add686e
Vinitpanchal12 <vinitjpanchal2002@gmail.com>
2023-12-15 21:17:53 +0530

    user and like model model

---
+++ src/models/like.js
@@ -0,0 +1,22 @@
+const mongoose =require('mongoose')
+
+const likeSchema = new mongoose.Schema({
+    onModel :{
+        type:String,
+        required: true,
+        enum : ['Tweet', 'Comment']
+    },
+    likeable:{
+        type: mongoose.Schema.Types.ObjectId,
+        required:true,
+        refPath:'onModel'
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    }
+},{timestamps:true});
+
+const Like = mongoose.model('Like', likeSchema);
+module.exports = Like;
